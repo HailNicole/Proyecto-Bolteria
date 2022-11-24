fc3ef0f31cdfeae7a0fa38f49702a00d2c655af8
gavilanes_stefany <sgavilanesc1@est.ups.edu.ec>
2022-11-24 13:33:47 -0500

    Cambios boleto

--- boleto.cpp
+++ boleto.cpp
@@ -1,6 +1,73 @@
 #include "boleto.h"
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+char Boleto::getFila() const
+{
+    return fila;
+};
+
+int Boleto::getColumna() const
+{
+    return columna;
+};
 
 Boleto::Boleto()
 {
+    this->sala=0;
+    this->fila=0;
+    this->columna=0;
+};
 
-}
+Boleto::Boleto(int sala, char fila, int columna, Pelicula _peliculas)
+{
+    this->sala=sala;
+    this->fila=toupper(fila);
+    this->columna=columna;
+    this->_peliculas=_peliculas;
+};
+
+
+void Boleto::mostrar_boleto()
+{
+    int espacios=_peliculas.getTitulo().length();
+    int a=44;
+
+    for(int i=0; i<=56; i++){
+        printf("%c", 205);
+    }
+      cout<<"\n";
+
+      printf("%c", 186); cout<<" ";
+      cout<<" Sala: "<<this->sala;
+      cout<<"\t\t\t\t\t\t"; printf("%c", 186); cout<<endl;
+
+      printf("%c", 186); cout<<" ";
+      cout<<" Hora: "<<this->_peliculas.getHora();
+      cout<<"\t\t\t\t\t"; printf("%c", 186); cout<<endl;
+
+      printf("%c", 186); cout<<" ";
+      cout<<" Asiento: "<<this->fila<<this->columna;
+      cout<<"\t\t\t\t\t\t"; printf("%c", 186); cout<<endl;
+
+      printf("%c", 186); cout<<" ";
+      cout<<" Pelicula: "<<this->_peliculas.getTitulo();
+
+      for(int i=1; i<a-espacios; i++){
+          cout<<"\u0020";
+      }
+
+      printf("%c", 186); cout<<endl;
+
+      for(int i=0; i<=56; i++){
+          printf("%c", 205);
+      }
+     cout<<"\n";
+
+};
+
+Boleto::~Boleto()
+{
+
+};
--- boleto.h
+++ boleto.h
@@ -1,11 +1,22 @@
 #ifndef BOLETO_H
 #define BOLETO_H
-
-
+#include "pelicula.h"
+#include "sala.h"
 class Boleto
 {
+private:
+    int sala;
+    Pelicula _peliculas;
+    Sala salas;
+    char fila;
+    int columna;
 public:
     Boleto();
+    Boleto(int, char, int, Pelicula);
+    char getFila() const;
+    int getColumna() const;
+    void mostrar_boleto();
+    ~Boleto();
 };
 
 #endif // BOLETO_H
